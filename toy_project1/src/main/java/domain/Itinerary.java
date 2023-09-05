fd125c2ac10c60ea4261d0e12e37e933a2048815
KwonMinwoo <qwerty029369@naver.com>
2023-09-05 12:01:35 +0900

    feat: 도메인 생성 완료
    프로젝트의 도메인으로 활용할 Trip (여행), Itinerary (여정) 을 완성함.

---
+++ toy_project1/src/main/java/domain/Itinerary.java
@@ -0,0 +1,81 @@
+package domain;
+
+public class Itinerary {
+    private int itineraryId;
+    private String departurePlace;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private String checkIn;
+    private String checkOut;
+
+
+    public Itinerary(int itineraryId, String departurePlace, String destination, String departureTime, String arrivalTime, String checkIn, String checkOut) {
+        this.itineraryId = itineraryId;
+        this.departurePlace = departurePlace;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Itinerary() {
+    }
+
+    public int getItineraryId() {
+        return itineraryId;
+    }
+
+    public void setItineraryId(int itineraryId) {
+        this.itineraryId = itineraryId;
+    }
+
+    public String getDeparturePlace() {
+        return departurePlace;
+    }
+
+    public void setDeparturePlace(String departurePlace) {
+        this.departurePlace = departurePlace;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+}
