984dcd7793588c6d54ce3eed60658a003c77a431
BackInGone <64956292+BackInGone@users.noreply.github.com>
2023-09-07 11:30:16 +0900

    Update SaveTripService.java
    fix : 
    - 파일저장메서드 예외처리 추가
    - setIdToTrip 간결화 및 여정ID입력내용 추가
    - CSV 헤더 입력 부분 하드코딩 분리
    - 각종 오타 수정

--- SaveTripService.java
+++ SaveTripService.java
@@ -2,6 +2,7 @@ package service;
 import model.Trip;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
@@ -11,15 +12,16 @@ public class SaveTripService {
 
     public void setIdToTrip(Trip trip, List<Trip> tripList) {
 
-        int maxID = 0;
+
+        int lastTripId = tripList.isEmpty() ? 0 : tripList.get(tripList.size() - 1).getTripId();
+        trip.setTripId(lastTripId + 1);
+        int itineraryId = 1;
 
         //최대ID값 다음 값으로 ID입력
-        for (int i = 0; i < tripList.size(); i++) {
-            if (tripList.get(i).getTripId() > maxID) {
-                maxID = tripList.get(i).getTripId();
-            }
+
+        for (Itinerary itinerary : trip.getItineraries()) {
+            itinerary.setItineraryId(itineraryId++);
         }
-        trip.setTripId(maxID + 1);
 
     }
 
@@ -27,40 +29,46 @@ public class SaveTripService {
     public void saveAsJSON(Trip trip) {
         ObjectMapper objectMapper = new ObjectMapper();
         //TripName_ID.json 으로 파일 생성 후 , 인코딩 설정
-        String filename = trip.getTripName() + "_" + trip.getTripId() + ".json";
-        Writer writer = new OutputStreamWriter(new FileOutputStream(filename, StandardCharsets.UTF_8);
-        objectMapper.writeValue(writer, trip);
+        String filename = "trip_" + trip.getTripId() + ".json";
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(filename, StandardCharsets.UTF_8))) {
+            objectMapper.writeValue(writer, trip);
 
-        writer.close();
+            moveFileToResources(filename);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
-        moveFileToResources(fileName);
     }
 
 
 
     public void saveAsCSV(Trip trip) {
+        String csvColumnName = "trip_id,trip_name,start_date,end_date,itinerary_id,departure,destination,departure_time,arrival_time,check_in,check_out\n";
         //TripName_ID.csv 으로 파일 생성 후 , 인코딩 설정
-        String filename = trip.getTripName() + "_" + trip.getTripId() + ".csv";
-        Writer writer = new OutputStreamWriter(new FileOutputStream(filename, StandardCharsets.UTF_8);
-        //CSV 헤더(컬럼이름) 작성
-        writer.append("trip_id,trip_name,start_date,end_date,itinerary_id,departure,destination,departure_time,arrival_time,check_in,check_out\n");
+        String filename = "trip_" + trip.getTripId() + ".csv";
+        try(Writer writer = new OutputStreamWriter(new FileOutputStream(filename, StandardCharsets.UTF_8))) {
+            //CSV 헤더(컬럼이름) 작성
+            writer.append(csvColumnName);
 
-        for (Itinerary itinerary : trip.getItineraries()) {
-            writer.append(trip.getTripId()).append(",")
-                    .append(trip.getTripName()).append(",")
-                    .append(trip.getStartDate()).append(",")
-                    .append(trip.getEndDate()).append(",")
-                    .append(String.valueOf(itinerary.getItineraryId())).append(",")
-                    .append(itinerary.getDeparture()).append(",")
-                    .append(itinerary.getDestination()).append(",")
-                    .append(itinerary.getDepartureTime()).append(",")
-                    .append(itinerary.getArrivalTime()).append(",")
-                    .append(itinerary.getCheckIn()).append(",")
-                    .append(itinerary.getCheckOut()).append("\n");
+            for (Itinerary itinerary : trip.getItineraries()) {
+                writer.append(trip.getTripId()).append(",")
+                        .append(trip.getTripName()).append(",")
+                        .append(trip.getStartDate()).append(",")
+                        .append(trip.getEndDate()).append(",")
+                        .append(String.valueOf(itinerary.getItineraryId())).append(",")
+                        .append(itinerary.getDeparture()).append(",")
+                        .append(itinerary.getDestination()).append(",")
+                        .append(itinerary.getDepartureTime()).append(",")
+                        .append(itinerary.getArrivalTime()).append(",")
+                        .append(itinerary.getCheckIn()).append(",")
+                        .append(itinerary.getCheckOut()).append("\n");
+            }
+            
+
+            moveFileToResources(fileName);
+        } catch (IOException e){
+            e.printStackTrace();
         }
-        writer.close();
-
-        moveFileToResources(fileName);
     }
 
 
@@ -77,17 +85,10 @@ public class SaveTripService {
 
 
 
-//todo 1. Itinerary가 입력받으면서 객체로 따로 분리될 필요는?
-//     2. [?] FileWriter와 Writer는 인코딩문제로  outputStreamWriter로 인코딩 지정하기 위해 Writer사용
-//     3. trip 모델 안에 addItinerary ? 어떤 형태로 여정을 받는지
-//        public void addItinerary(Itinerary itinerary) {
-//        this.itineraries.add(itinerary);}
-//     4. 테스트용이성 ?
-//     5. gradle 빌드 : OpenCSV
-//     6. resource 저장 상대경로 , 및 파일 이동 후 예외처리
-//     7. enum이나 상수 사용 방법
-//     8. departure이 arrival 보다 나중인지, check_in이 check_out보다 나중인지 유효성 체크?
-//     9. Viewer에서 날짜 시간 입력받을때, DataFormatter 사용하는지?.?
-//    10. 파일 네이밍 방식 적절 여부
-//    11. 2-3) 예외처리 -->>  JSON, CSV 형식에 맞지 않으면 오류 메세지를 출력한다.??
+//todo 1. 테스트용이성 ?
+//     2. gradle 빌드 : OpenCSV
+//     3. resource 저장 상대경로 , 및 파일 이동 후 예외처리
+//     4. enum이나 상수 사용 방법
+//     5. departure이 arrival 보다 나중인지, check_in이 check_out보다 나중인지 유효성 체크?
+//     6. 2-3) 예외처리 -->>  JSON, CSV 형식에 맞지 않으면 오류 메세지를 출력한다.??
 
