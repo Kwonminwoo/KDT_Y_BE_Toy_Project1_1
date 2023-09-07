1d26375d3382dfbeb5e7a704f0f8b634ae8239ee
pabu <socks1583@gmail.com>
2023-09-07 11:13:12 +0900

    feat: viewer 날짜,시각 입력 예시 추가
    viewer 날짜,시각 입력 예시 추가

--- toy_project1/src/main/java/view/Viewer.java
+++ toy_project1/src/main/java/view/Viewer.java
@@ -9,35 +9,24 @@ import java.util.Scanner;
 public class Viewer {
     Scanner sc = new Scanner(System.in);
 
-    public int receiveMenuSelectOption() {
+    public void printMenuOption() {
         System.out.println("---------------------------------------");
         System.out.println("#여행 여정을 기록과 관리하는 SNS 서비스#");
         System.out.println("[메인 메뉴] 여행기록(1) 여행조회(2) 종료(3)");
+    }
 
-        while (true) {
-            try {
-                System.out.print("시작할 메뉴번호를 입력 하세요: ");
-                int selectedMenuOption = sc.nextInt();
-                sc.nextLine();
-                if (selectedMenuOption < 1 || selectedMenuOption > 3) {
-                    System.out.println("메뉴 리스트에 있는 번호를 입력해주세요.");
-                    continue;
-                }
-                return selectedMenuOption;
-            } catch(Exception e){
-                System.out.println("정수 번호를 입력해야 합니다.");
-                sc.nextLine();
-            }
-        }
+    public String receiveMenuSelectOption() {
+        System.out.print("시작할 메뉴번호를 입력 하세요: ");
+        return sc.nextLine();
     }
 
     public void receiveTripInfo(Trip trip) {
         System.out.println("---여행정보 입력---");
         System.out.print("여행이름: ");
         trip.setTripName(sc.nextLine());;
-        System.out.print("시작날짜: ");
+        System.out.println("여행 시작날짜 입력\t(날짜 입력예시: 20230101)");
         trip.setStartDate(sc.nextLine());
-        System.out.print("종료날짜: ");
+        System.out.println("여행 종료날짜 입력\t(날짜 입력예시: 20230101)");
         trip.setEndDate(sc.nextLine());
     }
 
@@ -47,103 +36,49 @@ public class Viewer {
         itinerary.setDeparturePlace(sc.nextLine());
         System.out.print("도착지: ");
         itinerary.setDestination(sc.nextLine());
-        System.out.print("출발시각: ");
+        System.out.print("출발시각 입력\t(시각 입력예시: 02030101-2030)");
         itinerary.setDepartureTime(sc.nextLine());
-        System.out.print("도착시각: ");
+        System.out.print("도착시각 입력\t(시각 입력예시: 02030101-2030)");
         itinerary.setArrivalTime(sc.nextLine());
-        System.out.print("체크인 시각: ");
+        System.out.print("체크인 시각 입력\t(시각 입력예시: 02030101-2030)");
         itinerary.setCheckIn(sc.nextLine());
-        System.out.print("체크아웃 시각: ");
+        System.out.print("체크아웃 시각 입력\t(시각 입력예시: 02030101-2030)");
         itinerary.setCheckOut(sc.nextLine());
     }
 
-    public int receiveIfAddItinerary() {
-        while (true) {
-            try {
-                System.out.println("[여정 추가여부] 여정 기록 종료(1) 다른 여정 기록(2)");
-                int selectedItineraryOption = sc.nextInt();
-                sc.nextLine();
-
-                if (selectedItineraryOption < 1 || selectedItineraryOption > 2) {
-                    System.out.println("리스트에 있는 번호를 입력해주세요.");
-                    continue;
-                }
-
-                return selectedItineraryOption;
-
-            } catch(Exception e){
-                System.out.println("정수 번호를 입력해야 합니다.");
-                sc.nextLine();
-            }
-        }
+    public String receiveIfAddItinerary() {
+        System.out.println("[여정 추가여부] 다른 여정 기록(1) 여정 기록 종료(2)");
+        System.out.print("여정 추가여부를 선택하세요: ");
+        return sc.nextLine();
     }
 
-    public int receiveIfAddTrip() {
-        while (true) {
-            try {
-                System.out.println("[여행 추가여부] 메인으로(1) 다른 여행 기록(2)");
-                int selectedTripOption = sc.nextInt();
-                sc.nextLine();
-
-                if (selectedTripOption < 1 || selectedTripOption > 2) {
-                    System.out.println("리스트에 있는 번호를 입력해주세요.");
-                    continue;
-                }
-
-                return selectedTripOption;
-
-            } catch(Exception e){
-                System.out.println("정수 번호를 입력해야 합니다.");
-                sc.nextLine();
-            }
-        }
+    public String receiveIfAddTrip() {
+        System.out.println("[여행 추가여부] 다른 여행 기록(1) 메인으로(2)");
+        System.out.print("여행 추가여부를 선택하세요: ");
+        return sc.nextLine();
     }
 
-    public int receiveFileType() {
-        while (true) {
-            try {
-                System.out.println("[불러올 파일 종류 선택] JSON(1) CSV(2)");
-                int selectedTripOption = sc.nextInt();
-                sc.nextLine();
-
-                if (selectedTripOption < 1 || selectedTripOption > 2) {
-                    System.out.println("리스트에 있는 번호를 입력해주세요.");
-                    continue;
-                }
-
-                return selectedTripOption;
-
-            } catch(Exception e){
-                System.out.println("정수 번호를 입력해야 합니다.");
-                sc.nextLine();
-            }
-        }
+    public String receiveFileType() {
+        System.out.println("[불러올 파일 종류 선택] JSON(1) CSV(2)");
+        System.out.print("파일 종류를 선택하세요: ");
+        return sc.nextLine();
     }
 
-    public int receiveTripId(List<Trip> trips) {
+    public void printTripList(List<Trip> trips) {
         System.out.println("---여행 리스트---");
         for (Trip trip : trips) {
             System.out.print(trip.getTripName() + " 여행   id: ");
             System.out.println(trip.getTripId());
         }
+    }
 
-        while (true) {
-            try {
-                System.out.print("여정을 보고싶은 여행 id를 입력하세요: ");
-                int tripId = sc.nextInt();
-                sc.nextLine();
-
-                return tripId;
-
-            } catch(Exception e){
-                System.out.println("정수 번호를 입력해야 합니다.");
-                sc.nextLine();
-            }
-        }
+    public String receiveTripId() {
+        System.out.print("여정을 보고싶은 여행 id를 입력하세요: ");
+        return sc.nextLine();
     }
 
 
-    public void printItineraryOfTrip(Trip trip) {
+    public void printTrip(Trip trip) {
         System.out.println("선택한 여행 " + trip.getTripName());
         System.out.println("여행 시작일: " + trip.getStartDate());
         System.out.println("여행 종료일: " + trip.getEndDate());
